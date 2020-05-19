5f70163ea4dc3aa20eb64ce5f141332775e1a7e0
rina-1 <cba.mac@icloud.com>
2020-05-19 03:27:40 +0000

    rina-login/logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+before_action :configure_permiited_parameters, if: :devise_controller?
+protect_from_forgery with: :exception
+protected
+    def after_sign_up_path_for(resource)
+        photoes_index_path
+    end
+    def after_sign_in_path_for(resource)
+    	photoes_index_path
+	end
+	def after_sign_out_path_for(resource)
+		photoes_index_path
+	end
+	def configure_permiited_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:nema, :email])
+	end
 end
